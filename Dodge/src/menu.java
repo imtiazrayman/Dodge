90b1110971c1206005f1424384ec83cfb535cf61
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-03-23 03:16:16 -0400

    Created Menu class, for home screen

---
+++ Dodge/src/menu.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public class menu {
+	public static Rectangle playButton = new Rectangle(Game.WIDTH/2 - 50 , 150, 100, 50);
+	public static Rectangle helpButton = new Rectangle(Game.WIDTH/2 - 50 , 250, 100, 50);
+	public static Rectangle quitButton = new Rectangle(Game.WIDTH/2 - 50, 350, 100, 50);
+
+	public static void render(Graphics g) {
+		
+		Graphics2D g2d = (Graphics2D) g; // casting g to graphics 2d because rectangles require g2d.
+		
+		
+		Font fnt0 = new Font("ZapfDingbats", Font.BOLD, 50);
+		g.setFont(fnt0);
+		g.setColor(Color.white);
+		g.drawString("Dodge", Game.WIDTH/2 - 70, 100);
+		
+		g2d.draw(playButton);
+		g2d.draw(helpButton);
+		g2d.draw(quitButton);
+		
+	}
+}
