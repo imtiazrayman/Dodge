2e2fd63cec85eba198ae954db674bf26404819c9
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-02 18:20:46 -0400

    New Updates

--- Dodge/src/HUD.java
+++ Dodge/src/HUD.java
@@ -22,10 +22,13 @@ public class HUD
         {
                 g.setColor(Color.gray);
                 g.fillRect(15, 15, 200, 32);
+                
                 g.setColor(new Color(75,(int) greenValue, 0));
                 g.fillRect(15, 15,(int) HEALTH * 2, 32);
+                
                 g.setColor(Color.white);
                 g.drawRect(15, 15, 200, 32);
+                
                 g.drawString("Score: " + score, 15, 64);
                 g.drawString("Level: " + level, 15, 80);       
         }
