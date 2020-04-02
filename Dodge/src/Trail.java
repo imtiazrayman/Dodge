ad1a88c0efa7ab6d6bee010da035d00b47a147e8
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-02 18:18:03 -0400

    updated trail

--- Dodge/src/Trail.java
+++ Dodge/src/Trail.java
@@ -12,12 +12,19 @@ public class Trail extends GameObject{
         private Color color;
         private int width, height;     
         public Trail(float x, float y, ID id, Color color, int width, int height,float life, Handler handler){
-                super(x, y, id);
-                this.handler = handler;
-                this.color = color;
-                this.width = width;
-                this.height = height;
-                this.life = life;
+               
+        	super(x, y, id);
+               
+        	this.handler = handler;
+               
+        	this.color = color;
+                
+        	this.width = width;
+               
+        	this.height = height;
+                
+        	this.life = life;
+        	
         }
         public void tick(){
                 if(alpha > life){
