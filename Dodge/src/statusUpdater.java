3fc980598bf3e1e7742528305023fb9355a1fb78
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-02 18:21:13 -0400

    Created Message box to display user selected modes

---
+++ Dodge/src/statusUpdater.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public class statusUpdater {
+	
+	// this class will create a status update of what the user selects based on their own preferences.
+	
+	public statusUpdater() {
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
