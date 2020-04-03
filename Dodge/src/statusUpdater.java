02425f4ddc584e40e699a18db9f257534f96ae2d
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-02 21:02:20 -0400

    This prints out the Sattus of the player Selection

--- Dodge/src/statusUpdater.java
+++ Dodge/src/statusUpdater.java
@@ -3,17 +3,38 @@ import java.awt.*;
 public class statusUpdater {
 	
 	// this class will create a status update of what the user selects based on their own preferences.
+	public String PlayerMode = "";
+	public String DifficultyMode = "";
+	public String statusString = "";
 	
-	public statusUpdater() {
+	public String statusUpdater() {
+		if(Game.difficulty == gameDifficultyId.NORMAL) {
+			DifficultyMode = "NORMAL";
+		}
+		if(Game.difficulty == gameDifficultyId.HARD) {
+			DifficultyMode = "HARD";
+		}
+		if(Game.difficulty == gameDifficultyId.INSANE) {
+			DifficultyMode = "INSANE";
+		}
+		
+		if(Game.playersInGame == playerCount.SINGLEPLAYER) {
+			PlayerMode = "SINGLE PLAYER";
+		}
+		if(Game.playersInGame == playerCount.MULTIPLAYER) {
+			PlayerMode = "MULTI PLAYER";
+		}
 		
 		
-		
-		
+		statusString = PlayerMode + "\n" +  DifficultyMode;
+	
+		return statusString;
 		
 	}
 	
 	
 	
 	
-
+	
+	
 }
