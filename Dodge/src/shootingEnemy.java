6a6f007781a87b2fea32a930f17db88410b65fb8
sagarvadalia <sv344@cornell.edu>
2020-04-03 17:51:27 -0400

    new enemy

---
+++ Dodge/src/shootingEnemy.java
@@ -0,0 +1,36 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+public class shootingEnemy extends GameObject{
+    private boolean shouldShoot;
+    private Handler handler;
+    public shootingEnemy(int x, int y, ID id, Handler handler, boolean shouldShoot){
+        super(x,y,id);
+        this.handler = handler;
+        velX = 5;
+        velY = 5;
+        this.shouldShoot = shouldShoot;
+    }
+    public Rectangle getBounds(){
+        return new Rectangle((int) x,(int) y, 16, 16);
+    }
+    public void tick(){
+        x += velX;
+        y += velY;
+        if(y <= 0 || y >= Game.HEIGHT - 32) velY *= -1;
+        if(x <= 0 || x >= Game.WIDTH - 16) velX *= -1;
+        handler.addObject(new Trail((int)x,(int) y, ID.Trail, Color.blue, 16, 16, 0.02f,handler));
+        if(this.shouldShoot){
+            this.shouldShoot = false;
+            handler.addObject(new Trail((int)x + velX,(int) y + velY, ID.Trail, Color.blue, 16, 16, 0.02f,handler));
+        }
+        else {
+            this.shouldShoot = true;
+        }
+    }
+    public void render(Graphics g){
+        g.setColor(Color.blue);
+        g.fillRect((int)x,(int)y,16,16);
+    }
+}
